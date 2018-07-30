e78aeeed3fe5c1847e7a68f629b5d326d2501240
piiih <philipe.paixao@pagar.me>
2018-07-30 17:35:11 -0300

    client: add function to bind apiKey to body

--- src/Client.php
+++ src/Client.php
@@ -8,12 +8,18 @@ class Client
 {
     private $baseUrl = 'https://api.pagar.me:443/1';
 
+    private $body = [];
+
     public function __construct($apiKey) {
         if (!validateAuthentication($apiKey)) {
             throw Exception('You must supply a valid Api Key');
         }
 
-        $this->apiKey = $apiKey;
+        $this->bindToBody($apiKey);
+    }
+
+    private function bindToBody($apiKey) {
+        return array_merge($body, ['apiKey' => $apiKey]);
     }
 
     private function validateAuthentication($apiKey) {
